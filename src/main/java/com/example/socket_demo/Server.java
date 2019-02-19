3d7d08531f8f1a24679853a4fcfd328a21e7e6fc
jason.kim <br2k@daum.net>
2019-02-19 10:45:18 +0900

    refactoring

--- src/main/java/com/example/socket_demo/Server.java
+++ src/main/java/com/example/socket_demo/Server.java
@@ -18,35 +18,33 @@ public class Server {
         try {
             mServerSocket = new ServerSocket(5555);
             System.out.println("서버 시작!!!");
-            // 스레드가 멈춰 있고
 
-            // 연결 요청이 들어오면 연결
             mSocket = mServerSocket.accept();
             System.out.println("클라이언트와 연결 됨");
 
             DataInputStream dis = new DataInputStream(mSocket.getInputStream());
             DataOutputStream dos = new DataOutputStream(mSocket.getOutputStream());
 
-            while(true) {
-                var arr = new ArrayList<Byte>();
-                var s = Stream.iterate(arr, a -> {
-                        try { a.add(dis.readByte()); } catch (IOException e) { e.printStackTrace(); }
-                        return a;
-                    })
-                    .takeWhile(a -> !hasLineFeed(a))
-                    .reduce((first, second) -> second)
-                    .get();
-
-                dos.write(toPrimitiveArray(s));
-                dos.flush();
+            while (true) {
+                echo(dis, dos);
             }
 
-        } catch (Throwable e) {
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try { mSocket.close(); } catch (IOException e) { e.printStackTrace(); }
             try { mServerSocket.close(); } catch (IOException e) { e.printStackTrace(); }
         }
+    }
+
+    private void echo(DataInputStream dis, DataOutputStream dos) throws IOException {
+        var arr = new ArrayList<Byte>();
+        while (!hasLineFeed(arr)) {
+            try { arr.add(dis.readByte()); } catch (IOException e) { e.printStackTrace(); }
+        }
+
+        dos.write(toPrimitiveArray(arr));
+        dos.flush();
     }
 
     private boolean hasLineFeed(List<Byte> a) {
